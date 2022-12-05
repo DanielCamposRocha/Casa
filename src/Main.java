243a505fa6396bb9a5232e7c30766fc32ee9b22a
Daniel <danielcamposrocha9@gmail.com>
2022-12-05 13:06:36 +0100

    mostrando el problema de date

--- src/Main.java
+++ src/Main.java
@@ -13,7 +13,12 @@ public class Main {
        /* Empleado p2= new Empleado("Pepe Pérez", -31,new Date(90,Calendar.FEBRUARY,1));
         System.out.println(p);*/
 
-        System.out.println("Si quier mostrar solo el nombre: "+p.nombre);
-        System.out.println("Si quiero mostrar solo la edad no puedo: "+p.edad);//necesito un  getter
+        System.out.println("Si quier mostrar solo el nombre: "+ p.getNombre());
+        System.out.println("Si quiero mostrar solo la edad no puedo: "+p.getEdad());
+        /*aunque fecha es immutable, su tipo no lo es  de modo que podria modificarse
+        * p.getFechaContratacion().getYear(10);*/
+        /*date e suna calse que solo almancena 2 digitos par años,*/
+        System.out.println("nueva fecha de contratacion: "+p.getFechaContratacion().getYear());
+
     }
 }
