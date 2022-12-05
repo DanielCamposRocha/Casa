5230e3b949c4ca9979de357d80f2e3b67141f5ce
Daniel <danielcamposrocha9@gmail.com>
2022-12-05 12:57:07 +0100

    mostrando el problema

--- src/Main.java
+++ src/Main.java
@@ -12,5 +12,8 @@ public class Main {
         //p.edad=-31;//arreglado edad no accesible
        /* Empleado p2= new Empleado("Pepe Pérez", -31,new Date(90,Calendar.FEBRUARY,1));
         System.out.println(p);*/
+
+        System.out.println("Si quier mostrar solo el nombre: "+p.nombre);
+        System.out.println("Si quiero mostrar solo la edad no puedo: "+p.edad);//necesito un  getter
     }
 }
